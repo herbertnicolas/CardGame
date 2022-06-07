f5a37946e399201a7123e58affc9ed75e52c45a0
herbertnicolas <hna@cin.ufpe.br>
2022-06-06 21:41:46 -0300

    :art: cardfrontback css atualizado

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,11 +1,18 @@
 import Card from "../Card";
 import './style.css';
+//deixando a funçao handleClick no escopo global, pra ser acessada
+window.cardFrontBack = {}
+window.cardFrontBack.handleClick = (event) => {
+    const $alvo = event.target.parentNode.parentNode.parentNode;
+    $alvo.classList.add('-active')
+}
 
 function CardFrontBack(){
 //funcao responsavel pelas faces de cada carta
+    
     return ( /*html*/
     `
-        <article class = "card-frente-verso">
+        <article class = "card-frente-verso" onClick="cardFrontBack.handleClick(event)">
             <div class="card -back">
                 ${Card("logo-js", "logo-js")}
             </div>
